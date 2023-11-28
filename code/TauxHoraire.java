fd3030d870211d9a5fae3215452d6db1123ce762
kenless1 <48138341+kenless1@users.noreply.github.com>
2023-11-28 18:54:49 -0500

    Update TauxHoraire.java

--- code/TauxHoraire.java
+++ code/TauxHoraire.java
@@ -1,19 +1,48 @@
 import java.util.ArrayList;
 
+/**
+ * La classe TauxHoraire représente les taux horaires de base et supplémentaires
+ * pour une certaine catégorie d'employés.
+ */
 public class TauxHoraire {
-    private ArrayList<Float> tauxBase;
-    private ArrayList<Float> tauxSupplementaire;
+    private ArrayList<Float> tauxBase; // Liste des taux horaires de base.
+    private ArrayList<Float> tauxSupplementaire; // Liste des taux horaires supplémentaires.
 
+    /**
+     * Construit un nouvel objet TauxHoraire avec les taux horaires de base et supplémentaires spécifiés.
+     *
+     * @param tauxBase          La liste des taux horaires de base.
+     * @param tauxSupplementaire La liste des taux horaires supplémentaires.
+     */
     public TauxHoraire(ArrayList<Float> tauxBase, ArrayList<Float> tauxSupplementaire) {
         this.tauxBase = tauxBase;
         this.tauxSupplementaire = tauxSupplementaire;
     }
 
-    public float obtenirTauxBaseCourant(){
-        return tauxBase.getLast();
+    /**
+     * Modifie le taux horaire de base courant en ajoutant un nouveau taux.
+     *
+     * @param taux Le nouveau taux horaire de base à ajouter.
+     */
+    public void modifierTauxBaseCourant(float taux) {
+        tauxBase.add(taux);
     }
 
-    public float obtenirTauxSupplementaireCourant(){
-        return tauxSupplementaire.getLast();
+    /**
+     * Obtient le taux horaire de base courant, c'est-à-dire le dernier taux horaire de base ajouté.
+     *
+     * @return Le taux horaire de base courant.
+     */
+    public float obtenirTauxBaseCourant() {
+        return tauxBase.get(tauxBase.size() - 1);
+    }
+
+    /**
+     * Obtient le taux horaire supplémentaire courant, c'est-à-dire le dernier taux horaire supplémentaire ajouté.
+     *
+     * @return Le taux horaire supplémentaire courant.
+     */
+    public float obtenirTauxSupplementaireCourant() {
+        return tauxSupplementaire.get(tauxSupplementaire.size() - 1);
     }
 }
