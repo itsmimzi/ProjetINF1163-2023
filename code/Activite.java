5ec113c0998e1966216cbd95ac12c0ce9f720f05
Maryam M <101988918+itsmimzi@users.noreply.github.com>
2023-12-13 14:45:19 -0500

    Update1 Activite.java

--- code/Activite.java
+++ code/Activite.java
@@ -1,61 +1,113 @@
-import java.sql.Time;
-import java.util.ArrayList;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+
+/*
+ * Classe ACTIVITE : 
+ * 
+ * Tout employé peut se connecter pour signaler qu’il a commencé ou terminé une activité.
+ * L’heure et date de début ou d’arrêt d’une activité sont enregistrées par le système.
+ * 
+ * Une activité est caractérisée :
+ *   par le nom du projet auquelle elle est associée,
+ *   une discipline de travail (design, implémentation, etc. ),
+ *   une seule activité est permise à la fois par employé. 
+ * 
+ * */
 
 public class Activite {
 
-    private String idActivite;
-    private Time heureDebut;
-    private Time heureFin;
-    private Employe employe // L'employé associé à cette activité
-    ArrayList<Discipline> disciplines = new ArrayList<>(1);
+    // Variable statique qui suit les affectations d'ID aux activités
+    private static int dernierIdAttribue = 0;
 
+    private int idActivite;
+    private String nomProjet;
+    private LocalDateTime heureDebut;
+    private LocalDateTime heureFin;
+    private Employe employe; 
+    private Discipline discipline;
 
-    public Activite(String idActivite, Time heureDebut, Time heureFin, ArrayList<Discipline> disciplines) {
-        this.idActivite = idActivite;
-        this.heureDebut = heureDebut;
-        this.heureFin = heureFin;
-        this.disciplines = disciplines;
-        this.employé = employé;
+    /**
+     * Constructeur de Classe
+     * 
+     * @param idActivite L'identifiant unique de l'activité
+     * @param employe L'employé qui a signaler cette activité
+     */
+
+    public Activite(int idActivite, Employe employe) {
+        this.idActivite = ++dernierIdAttribue;        
+        this.employe = employe;
+    }
+    
+    /*********************************************************************************************************
+     **************************************** ACCESSEURS & MODIFICATEURS *************************************
+     *********************************************************************************************************/
+    
+     public int getIdActivite(){
+        return idActivite;
     }
 
-    // Getters and setters
-     public List<Discipline> getDisciplines() {
-        return disciplines;
+    public Employe getEmploye() {
+        return employe;
     }
 
-    public void setDisciplines(List<Discipline> disciplines) {
-        this.disciplines = disciplines;
+    public String getNomProjet(){
+        return nomProjet;
     }
 
-    public Time getHeureDebut() {
+    public LocalDateTime getHeureDebut(){
         return heureDebut;
     }
 
-    public void setHeureDebut(Time heureDebut) {
-        this.heureDebut = heureDebut;
-    }
-
-    public Time getHeureFin() {
+    public LocalDateTime getHeureFin(){
         return heureFin;
     }
 
-    public void setHeureFin(Time heureFin) {
-        this.heureFin = heureFin;
+    public void getDiscipline() {
+       return discipline;
     }
 
-    public Employé getEmployé() {
-        return employé;
+
+    /**
+     * Signaler le debut d'une activité avec l'heure et la date locale du systeme.
+     * 
+     * @param nomProjet Projet auquel est associée l'activité
+     * @param discipline Discipline de l'activité
+     */
+    public void signalerDebutActivite(String nomProjet, Discipline discipline) {
+        if (heureDebut == null && heureFin == null) {
+            this.nomProjet = nomProjet;
+            this.discipline = discipline;
+            this.heureDebut = LocalDateTime.now();
+        } else {
+            System.out.println("L'employé a déjà une activité en cours.");
+        }
     }
 
-    public void setEmployé(Employé employé) {
-        this.employé = employé;
+    /**
+     * Signaler la fin d'une activité avec l'heure et la date locale du système.
+     */
+    public void signalerFinActivite() {
+        if (heureDebut != null && heureFin == null) {
+            this.heureFin = LocalDateTime.now();
+        } else {
+            System.out.println("L'employé n'a pas d'activité en cours.");
+        }
     }
 
-    // Méthode pour obtenir le temps écoulé en millisecondes
-    public float obtenirTempsEcouler(){
-        float tempsEcouler = heureFin.getTime() -heureDebut.getTime();
-        return (float)tempsEcouler / 3600000.0f;
+    /**
+     * Calculer la durée d'une activité en fonction de son temps de début et de fin.
+     * 
+     * @return La durée de l'activité
+     */
+    public long calculerHeuresTravaillees() {
+        if (heureDebut != null && heureFin != null) {
+            Duration duree = Duration.between(heureDebut, heureFin);
+            return duree.toHours();
+        } else {
+            System.out.println("L'activité n'est pas complète. Impossible de calculer les heures travaillées.");
+            return 0;
+        }
     }
 
-    
 }
