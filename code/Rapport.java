236ba7d56ab16b04d1abb0615cdb99bf530d1697
Maryam M <101988918+itsmimzi@users.noreply.github.com>
2023-12-13 15:49:53 -0500

    Update1 Rapport.java

--- code/Rapport.java
+++ code/Rapport.java
@@ -1,3 +1,20 @@
-public class Rapport {
+/*
+ * Classe GENERATEUR RAPPORT : 
+ * 
+ * Permet de générer 
+ *    un rapport d’état de chaque projet choisi, 
+ *    un rapport d’état global de l’ensemble des projets,
+ *    un rapport des valeurs (en salaire) des heures travaillées de l’employé; 
+ *       avec par défaut depuis le début de la dernière semaine impaire. 
+ *       Ce rapport donne le salaire brut pour la période choisie, pour chacun des projets sur lesquels 
+ *       l’employé a travaillé durant la période choisie.
+ *    un rapport  des totaux des salaires (bruts et nets) de tous les employés,
+ *       il fournit donc seulement deux valeurs; total des salaires brutes et total des salaires nets.
+ * 
+ * */
+
+public class generateurRapport {
+
+    
     
 }
