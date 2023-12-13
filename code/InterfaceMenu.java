e7b03d82d10bc0fa4bced40176d15ae8dc59536d
kenless1 <48138341+kenless1@users.noreply.github.com>
2023-12-12 22:41:07 -0500

    Update InterfaceMenu.java
    Ajout d'interface de menu et de demande de input.

--- code/InterfaceMenu.java
+++ code/InterfaceMenu.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class InterfaceMenu {
 
-    public static void main(String[] args) {
+    public static void interfaceMenuRapport() {
         Scanner scanner = new Scanner(System.in);
         int choix;
 
         do {
             // Afficher le menu
-            System.out.println("=== Menu TimeLog ===");
+            System.out.println("=== Menu Rapport ===");
             System.out.println("1. Rapport d'etat d'un projet choisi");
             System.out.println("2. Rapport d'etat global");
             System.out.println("3. Rapport des salaires d'un employe");
@@ -20,7 +20,7 @@ public class InterfaceMenu {
             // Lire le choix de l'utilisateur
             choix = scanner.nextInt();
 
-            // ExÈcuter l'action correspondante
+            // Ex√©cuter l'action correspondante
             switch (choix) {
                 case 1:
                     rapportEtatProjet();
@@ -50,27 +50,76 @@ public class InterfaceMenu {
     }
 
     private static void rapportEtatProjet() {
-        // ImplÈmenter la fonctionnalitÈ
+        // Impl√©menter la fonctionnalit√©
         System.out.println("Fonctionnalite non implementee : Rapport d'etat d'un projet choisi");
     }
 
     private static void rapportEtatGlobal() {
-        // ImplÈmenter la fonctionnalitÈ
+        // Impl√©menter la fonctionnalit√©
         System.out.println("Fonctionnalite non implementee : Rapport d'etat global");
     }
 
     private static void rapportSalairesEmploye() {
-        // ImplÈmenter la fonctionnalitÈ
+        // Impl√©menter la fonctionnalit√©
         System.out.println("Fonctionnalite non implementee : Rapport des salaires d'un employe");
     }
 
     private static void talonPaieEmploye() {
-        // ImplÈmenter la fonctionnalitÈ
+        // Impl√©menter la fonctionnalit√©
         System.out.println("Fonctionnalite non implementee : Talon de paie d'un employe");
     }
 
     private static void totauxSalaires() {
-        // ImplÈmenter la fonctionnalitÈ
+        // Impl√©menter la fonctionnalit√©
         System.out.println("Fonctionnalite non implementee : Totaux des salaires bruts et nets");
+    }
+
+    public static void menuEmployeConnecter(){
+        System.out.println("Veuillez entrer voitre choix: ");
+        System.out.println("1. Demarer une activite.");
+        System.out.println("2. Terminer une activite.");
+    }
+
+    public static String demandeNomUtilisateur(){
+        System.out.println("Veuillez entrer votre nom d'utilisateur: ");
+        String nomUtilisateur = new Scanner(System.in).nextLine();
+        return nomUtilisateur;
+    }
+
+    public static int demandeIDUtilisateur(){
+        System.out.println("Veuillez entrer votre ID: ");
+        int idUtilisateur = Integer.parseInt(new Scanner(System.in).nextLine());
+        return idUtilisateur;
+    }
+
+    /**
+     * Affiche les options de menu √† l'utilisateur et lit son choix.
+     * Cette m√©thode affiche un menu de quatre options et attend que l'utilisateur entre un choix.
+     * Le choix est lu √† partir de l'entr√©e standard et renvoy√©.
+     *
+     * @return Le choix de l'utilisateur sous forme d'entier.
+     */
+    public static int choixMenu(){
+        Scanner choixScan = new Scanner(System.in);
+        System.out.println("    1. Connection Employe");
+        System.out.println("    2. Connection Administrateur");
+        System.out.println("    3. Option");
+        System.out.println("    4. Quiter");
+        System.out.print("Votre choix en entier: ");
+        int choix = Integer.parseInt(choixScan.nextLine());
+        
+        return choix;
+    }
+
+    public static String demandeNomProjet(){
+        System.out.println("Veuillez entrer le nom du projet: ");
+        String nomProjet = new Scanner(System.in).nextLine();
+        return nomProjet;
+    }
+
+    public static String demandeDiscipline(){
+        System.out.println("Veuillez entrer la discipline: ");
+        String discipline = new Scanner(System.in).nextLine();
+        return discipline;
     }
 }
