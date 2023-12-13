a28ff86bdbfa45b5221d2229e15c3ff8b14f392e
kenless1 <48138341+kenless1@users.noreply.github.com>
2023-12-12 22:41:38 -0500

    Update Program.java

--- code/Program.java
+++ code/Program.java
@@ -5,6 +5,11 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Program {
+    TimeLog timeLog = new TimeLog();
+    public static void main(String[] args) {
+        Program program = new Program();
+        program.executionProgramm();
+    }
 
     /**
      * Point d'entrée principal du programme.
@@ -13,49 +18,12 @@ public class Program {
      *
      * @param args Arguments de ligne de commande non utilisés dans cette application.
      */
-    public static void main(String[] args) {
-        menu();
+    public void executionProgramm(){
+        timeLog.ajouterEmployer(new Employe(1, "employe1", "Nemploye1", 111222333, new Date(), null));
+        timeLog.ajouterEmployer(new Employe(2, "employe2", "Nemploye2", 111222334, new Date(), null));
+        timeLog.ajouterEmployer(new Employe(3, "employe3", "Nemploye3", 111222335, new Date(), null));
+        timeLog.menuTimeLog();
     }
 
-    /**
-    * Affiche le menu principal du programme et gère la navigation de l'utilisateur.
-    * Cette méthode affiche un menu de console répétitif jusqu'à ce que l'utilisateur choisisse de quitter.
-    * À chaque itération, elle invite l'utilisateur à faire un choix, traite ce choix via la méthode
-    * {@link #traitementMenu(int)} et vérifie si l'utilisateur a choisi de quitter le programme.
-    */
-    public static void menu(){
-        System.out.println("-- Bienvenue sur le programme TimeLog --\n");
-        int choixMenu = -1;
-        while (choixMenu != 4) {  
-            System.out.println("Veuillez faire un choix et entre le numero du menu (1-4): ");
-            choixMenu = choixMenu(); 
-            traitementMenu(choixMenu);
-        }
-    }
-
-     /**
-     * Affiche les options de menu à l'utilisateur et lit son choix.
-     * Cette méthode affiche un menu de quatre options et attend que l'utilisateur entre un choix.
-     * Le choix est lu à partir de l'entrée standard et renvoyé.
-     *
-     * @return Le choix de l'utilisateur sous forme d'entier.
-     */
-    public static int choixMenu(){
-        Scanner choixScan = new Scanner(System.in);
-        System.out.println("    1. Connection Employe");
-        System.out.println("    2. Option");
-        System.out.println("    3. Option");
-        System.out.println("    4. Quiter");
-        System.out.print("Votre choix en entier: ");
-        int choix = Integer.parseInt(choixScan.nextLine());
-        return choix;
-    }
-
-
-
-    public static void traitementMenu(int choixMenu){
-        
-    }
-
-
+    
 }
