03dafced22e3d16f9ec6b155aeea5ed1131bf056
kenless1 <48138341+kenless1@users.noreply.github.com>
2023-12-13 17:08:05 -0500

    Update TimeLog.java

--- code/TimeLog.java
+++ code/TimeLog.java
@@ -1,4 +1,6 @@
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class TimeLog {
@@ -31,21 +33,74 @@ public class TimeLog {
         return false;
     }
 
+    /**
+    * Démarre une activité pour un employé basé sur l'ID utilisateur donné.
+    *
+    * @param idUtilisateur L'identifiant de l'utilisateur (employé) qui démarre l'activité.
+    *
+    * Ce méthode effectue les étapes suivantes :
+    * 1. Demande le nom du projet et la discipline via l'interface de menu.
+    * 2. Récupère les informations de l'employé courant en utilisant son ID.
+    * 3. Tente de récupérer le projet actuel en utilisant le nom du projet.
+    * 4. Si le projet existe :
+    *    a. Vérifie si l'employé courant a déjà une activité en cours.
+    *    b. Si aucune activité n'est en cours pour cet employé :
+    *       i. Crée une nouvelle activité avec l'ID de l'utilisateur et les informations de l'employé.
+    *       ii. Signale le début de l'activité avec le nom du projet et la discipline.
+    *       iii. Ajoute la nouvelle activité au projet courant.
+    *       iv. Affiche un message confirmant le début de l'activité.
+    *       v. Met à jour l'activité courante de l'employé avec la nouvelle activité.
+    *    c. Si une activité est déjà en cours pour cet employé, affiche un message indiquant qu'une activité est déjà en cours.
+    * 5. Si le projet n'existe pas, affiche un message indiquant que le projet n'existe pas.
+    */
     public void demarerActivite(int idUtilisateur){
         String nomProjet = InterfaceMenu.demandeNomProjet();
         String discipline = InterfaceMenu.demandeDiscipline();
         Employe employeCourant = obtenirEmploye(idUtilisateur);
-        
-        // Regardersi emplye a deja une activite en cours
-        // ajouter activite si non.
-        // TODO
+        Projet projetCourant = obtenirProjet(nomProjet);
+        if(projetCourant != null){
+            // Verifier si activite deja 
+            if(employeCourant.getActiviteCourant() == null){
+                Activite nouvelleActivite = new Activite(idUtilisateur, employeCourant);
+                nouvelleActivite.signalerDebutActivite(nomProjet, discipline);
+                projetCourant.ajouterActivite(nouvelleActivite);
+                System.out.println("Activite commencer "+nouvelleActivite.getIdActivite());
+                employeCourant.setActiviteCourant(nouvelleActivite);
+            }
+            else{
+                System.out.println("Activite deja en cours pour cette employe.");
+            }
+            
+        }else{
+            System.out.println("Projet non existant.");
+        }
     }
 
-    public void terminerActivite(){
-        System.out.println("Terminer activite.");
-        // Terminer une activite
-        // TODO
-
+    /**
+    * Termine l'activité en cours pour un employé spécifié par son ID utilisateur.
+    *
+    * @param idUtilisateur L'identifiant de l'utilisateur (employé) dont l'activité doit être terminée.
+    *
+    * Cette méthode effectue les opérations suivantes :
+    * 1. Récupère les informations de l'employé courant en utilisant son ID.
+    * 2. Vérifie si l'employé courant a une activité en cours.
+    * 3. Si aucune activité n'est en cours pour cet employé :
+    *    a. Affiche un message indiquant qu'il n'y a aucune activité en cours.
+    * 4. Si une activité est en cours :
+    *    a. Signale la fin de l'activité en cours.
+    *    b. Réinitialise l'activité courante de l'employé à null.
+    *    c. Affiche un message confirmant la fin de l'activité en cours.
+    */
+    public void terminerActivite(int idUtilisateur){
+        Employe employeCourant = obtenirEmploye(idUtilisateur);
+        if(employeCourant.getActiviteCourant()==null){
+            System.out.println("Aucune activite en cours.");
+        }
+        else{
+            employeCourant.getActiviteCourant().signalerFinActivite();
+            employeCourant.setActiviteCourant(null);
+            System.out.println("Activite en cours terminer.");
+        }
     }
 
     public void menuTimeLog(){
@@ -102,7 +157,7 @@ public class TimeLog {
             if(choix == 1){
                 demarerActivite(idUtilisateur);
             }else if(choix == 2){
-                terminerActivite();
+                terminerActivite(idUtilisateur);
             }
             // Ajouter autre choix
             // TODO
@@ -118,4 +173,14 @@ public class TimeLog {
         }
         return null;
     }
+
+    public Projet obtenirProjet(String nomProjet){
+        for (Projet projet : listeProjets) {
+            if(projet.getNomProjet().equals(nomProjet)){
+                return projet;
+            }
+        }
+        return null;
+    }
 }
+
