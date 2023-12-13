63f98e4fc289bd85e53fe54e171aaf844c5ad08b
Maryam M <101988918+itsmimzi@users.noreply.github.com>
2023-12-13 13:39:09 -0500

    Update1 Administrateur.java

--- code/Administrateur.java
+++ code/Administrateur.java
@@ -5,10 +5,10 @@ public class Administrateur extends Employe{
     private String motPasse;
     private String nomUtilisateur;
 
-    public Administrateur(int idEmploye, String prenomEmploye, String nomEmploye, int nas, Date dateEmbauche,
+    public Administrateur(int idEmploye, String nomEmploye, int nas, Date dateEmbauche,
             Date dateDepart, Poste poste, TauxHoraire tauxHoraire, ArrayList<Activite> activites,
             ArrayList<Projet> projets) {
-        super(idEmploye, prenomEmploye, nomEmploye, nas, dateEmbauche, dateDepart, poste, tauxHoraire, activites, projets);
+        super(idEmploye, nomEmploye, nas, dateEmbauche, dateDepart, poste, tauxHoraire, activites, projets);
     }
     
     
