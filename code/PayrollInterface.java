3ef1d4cb86f87cda2b3f96b499acb12e53537188
darinjorel <147176063+darinjorel@users.noreply.github.com>
2023-12-14 02:34:17 +0100

    Update PayrollInterface.java

--- code/PayrollInterface.java
+++ code/PayrollInterface.java
@@ -1,4 +1,21 @@
+import java.util.List;
+
 public interface PayrollInterface {
-    public float netFromBrute();
-    public Rapport DeductionRapport();
+
+    /**
+     * Calcule le salaire net total à partir des informations de paie.
+     */
+   public float netFromBrute(List<PayInfo> payInfos);
+
+    /**
+     * Génère un rapport de déductions pour une liste d'informations de paie.
+     */
+   public Rapport deductionRapport(List<PayInfo> payInfos);
+
+    /**
+     * Affiche les chèques de paie pour une liste d'informations de paie.
+     */
+    public void printPay(List<PayInfo> payInfos);
+
 }
+
